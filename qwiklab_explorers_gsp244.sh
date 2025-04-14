bdfdb47382931fbd39236d5287e4b9e4fac9efe3
QwikLab Explorers <155870248+Titash-shil@users.noreply.github.com>
2025-04-14 23:57:04 +0530

    Create qwiklab_explorers_gsp244.sh

---
+++ qwiklab_explorers_gsp244.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+# Define text colors and formatting
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+
+RESET_FORMAT=$'\033[0m'
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+clear # Clear the terminal screen
+
+# --- Script Header ---
+
+
+echo -n "${YELLOW_TEXT}${BOLD_TEXT}Enter your lab zone: ${RESET_FORMAT}"
+read ZONE
+export ZONE
+
+# Enable the required API
+gcloud services enable file.googleapis.com
+
+# Create a Compute Engine instance
+gcloud compute instances create nfs-client \
+--project=$DEVSHELL_PROJECT_ID --zone=$ZONE --machine-type=e2-medium \
+--network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
+--metadata=enable-oslogin=true \
+--maintenance-policy=MIGRATE \
+--provisioning-model=STANDARD \
+--scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/trace.append --tags=http-server \
+--create-disk=auto-delete=yes,boot=yes,device-name=nfs-client,image=projects/debian-cloud/global/images/debian-11-bullseye-v20231010,mode=rw,size=10,type=projects/$DEVSHELL_PROJECT_ID/zones/$ZONE/diskTypes/pd-balanced --no-shielded-secure-boot --shielded-vtpm --shielded-integrity-monitoring --labels=goog-ec-src=vm_add-gcloud --reservation-affinity=any
+
+# Create a Filestore instance
+gcloud filestore instances create nfs-server \
+--zone=$ZONE --tier=BASIC_HDD \
+--file-share=name="vol1",capacity=1TB \
+--network=name="default"
+
+# Final message
+echo -e "${RED_TEXT}${BOLD_TEXT}Subscribe my Channel (QwikLab Explorers):${RESET_FORMAT} ${BLUE_TEXT}${BOLD_TEXT}https://www.youtube.com/@qwiklabexplorers${RESET_FORMAT}"
+echo
